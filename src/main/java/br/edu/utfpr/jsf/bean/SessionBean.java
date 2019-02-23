84e288e7fda96ba9a5239cdd0318c265897d25b1
João Guilherme Brasil Pichetti <joaopichetti@joao-pc>
2019-02-23 14:01:58 -0300

    Alteração de tema em tempo de execução

---
+++ src/main/java/br/edu/utfpr/jsf/bean/SessionBean.java
@@ -0,0 +1,72 @@
+package br.edu.utfpr.jsf.bean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.edu.utfpr.jsf.util.Theme;
+
+@ManagedBean
+@SessionScoped
+public class SessionBean {
+	
+	private String theme = "bootstrap";
+	private List<Theme> themes = Arrays.asList(
+			new Theme("Afterdark", "afterdark"),
+			new Theme("Afternoon", "afternoon"),
+			new Theme("Black-tie", "black-tie"),
+			new Theme("Blitzer", "blitzer"),
+			new Theme("Bluesky", "bluesky"),
+			new Theme("Bootstrap", "bootstrap"),
+			new Theme("Casablanca", "casablanca"),
+			new Theme("Cruze", "cruze"),
+			new Theme("Cupertino", "cupertino"),
+			new Theme("Dark-hive", "dark-hive"),
+			new Theme("Delta", "delta"),
+			new Theme("Dot-luv", "dot-luv"),
+			new Theme("Eggplant", "eggplant"),
+			new Theme("Excite-bike", "excite-bike"),
+			new Theme("Flick", "flick"),
+			new Theme("Glass-x", "glass-x"),
+			new Theme("Home", "home"),
+			new Theme("Hot-sneaks", "hot-sneaks"),
+			new Theme("Humanity", "humanity"),
+			new Theme("Le-frog", "le-frog"),
+			new Theme("Midnight", "midnight"),
+			new Theme("Mint-choc", "mint-choc"),
+			new Theme("Omega", "omega"),
+			new Theme("Overcast", "overcast"),
+			new Theme("Pepper-grinder", "pepper-grinder"),
+			new Theme("Redmond", "redmond"),
+			new Theme("Rocket", "rocket"),
+			new Theme("Sam", "sam"),
+			new Theme("Smoothness", "smoothness"),
+			new Theme("South-street", "south-street"),
+			new Theme("Start", "start"),
+			new Theme("Sunny", "sunny"),
+			new Theme("Swanky-purse", "swanky-purse"),
+			new Theme("Trontastic", "trontastic"),
+			new Theme("Ui-darkness", "ui-darkness"),
+			new Theme("Ui-lightness", "ui-lightness"),
+			new Theme("Vader", "vader")
+	);
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	
+	public List<Theme> getThemes() {
+		return themes;
+	}
+
+	public void setThemes(List<Theme> themes) {
+		this.themes = themes;
+	}
+
+}
