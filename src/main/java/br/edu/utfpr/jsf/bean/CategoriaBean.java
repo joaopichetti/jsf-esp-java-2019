824e049a0f28d45ac81768353b2b3f14a09bb8af
João Guilherme Brasil Pichetti <joaopichetti@joao-pc>
2019-02-23 13:01:36 -0300

    Resolução atividade 03

--- src/main/java/br/edu/utfpr/jsf/bean/CategoriaBean.java
+++ src/main/java/br/edu/utfpr/jsf/bean/CategoriaBean.java
@@ -4,22 +4,29 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.SelectEvent;
 
 import br.edu.utfpr.jsf.dao.DAO;
 import br.edu.utfpr.jsf.model.Categoria;
 import br.edu.utfpr.jsf.util.FacesUtil;
 
 @ManagedBean
+@ViewScoped
 public class CategoriaBean {
 	
 	private Categoria categoria;
 	private List<Categoria> categorias;
 	private DAO<Categoria> dao;
+	private Operacao operacao;
+	private boolean categoriaSelecionada;
 	
 	@PostConstruct
 	public void inicializar() {
 		dao = new DAO<>(Categoria.class);
 		categoria = new Categoria();
+		operacao = Operacao.LISTAR;
 		listar();
 	}
 	
@@ -39,10 +46,16 @@ public class CategoriaBean {
 		this.categorias = categorias;
 	}
 
+	public boolean isCategoriaSelecionada() {
+		return categoriaSelecionada;
+	}
+
+	public void setCategoriaSelecionada(boolean categoriaSelecionada) {
+		this.categoriaSelecionada = categoriaSelecionada;
+	}
+
 	public void alterar() {
-		if (categoria == null) {
-			FacesUtil.addMensagemErro("Selecione uma categoria");
-		}
+		operacao = Operacao.EDITAR;
 	}
 
 	private void listar() {
@@ -51,6 +64,7 @@ public class CategoriaBean {
 	
 	public void novo() {
 		categoria = new Categoria();
+		operacao = Operacao.INSERIR;
 	}	
 	
 	public void remover() {
@@ -61,6 +75,7 @@ public class CategoriaBean {
 		}
 		novo();
 		listar();
+		categoriaSelecionada = false;
 	}
 	
 	public void salvar() {
@@ -71,6 +86,28 @@ public class CategoriaBean {
 		}
 		novo();
 		listar();
+		operacao = Operacao.LISTAR;
+		categoriaSelecionada = false;
+	}
+	
+	public void voltarParaListagem() {
+		categoria = new Categoria();
+		operacao = Operacao.LISTAR;
+		categoriaSelecionada = false;
+	}
+	
+	public String getTituloForm() {
+		return Operacao.EDITAR == operacao ? 
+				"Alterar Categoria" :
+				"Nova Categoria";
+	}
+	
+	public void onRowSelect(SelectEvent event) {
+		categoriaSelecionada = true;
+	}
+	
+	public boolean mostrarForm() {
+		return Operacao.LISTAR != operacao;
 	}
 
 }
